a2643707c516aed509b3ba7995985ae29d4c21ca
Kishore Narendran <kishore.narendran09@gmail.com>
2017-03-03 10:20:27 -0800

    Updates the tfidf script

--- tfidf.py
+++ tfidf.py
@@ -14,7 +14,7 @@ with open(prefix + 'bookkeeping.json', 'r') as file_handle:
 
 N = len(urls)
 
-i = 0
+count = 0
 for term in index.find():
     df_t = len(term['documents'])
     idf_t = log(N/df_t, 10)
@@ -24,6 +24,10 @@ for term in index.find():
         tf_idf = tf * idf_t
         term['documents'][i]['tf_idf'] = tf_idf
 
-    print term['token'], ' TF-IDF calculation'
+    # print term['token'], ' TF-IDF calculation'
     index.update({'token': term['token']}, {"$set": term}, upsert=False)
+
+    count += 1
+    if count % 1000 == 0:
+        print count, ' terms have been evaluated'
 
